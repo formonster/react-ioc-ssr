9c8fd824c9d328e65707e38fcb4544ab40eb4212
root <1739237939@qq.com>
2021-02-26 13:31:28 +0800

    新增：工具库

--- src/server/utils/common.ts
+++ src/server/utils/common.ts
@@ -4,3 +4,4 @@ import UserModel from "@/models/UserModel";
 export const nowTimeSecond = () => Math.floor(new Date().getTime() / 1000);
 // 检查服务端返回结果是否有异常
 export const checkServerResult = (res: ResponseData<UserModel>) => res.status !== 200;
+export const isNullObject = (o: object) => JSON.stringify(o) === "{}";
